ef77cd6d24c014258f245f482e7f821aad68e649
Chi Huynh <chi@skyhawk-277gt.lan>
2019-10-07 08:42:28 -0700

    search box created

---
+++ src/pages/Container.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import SearchBox from "./SearchBox"
+
+class Container extends Component {
+	state = {  }
+	render() { 
+		return (
+			<>
+			<SearchBox/>
+
+			</>
+			
+		  );
+	}
+}
+ 
+export default Container;
---
+++ src/pages/SearchBox.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class SearchBox extends Component {
+	state = {  }
+	render() { 
+		return ( 
+			<>
+			<label>Search: </label>
+			<input text='hello'/>
+			</>
+		 );
+	}
+}
+ 
+
+ 
+export default SearchBox;
+
+
