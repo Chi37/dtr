ee241659d5f0565979c4aed39d454b63499a6062
Chi Huynh <chi@skyhawk-277gt.local>
2019-10-09 11:36:32 -0700

    fix spacing

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -7,35 +7,36 @@ let url = "https://en.wikipedia.org/w/api.php";
 
 
 export function fetchWiki(input) {
-	const params = {
-		action: "opensearch",
-		search: input,
-		limit: "1",
-		namespace: "0",
-		format: "json"
-	};
-	url = url + "?origin=*";
-	Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
-	return fetch(url, { mode: 'cors' })
-		.then(function (response) { return response.json(); })
-		.catch(function (error) { console.log(error); });
+  const params = {
+    action: "opensearch",
+    search: input,
+    limit: "1",
+    namespace: "0",
+    format: "json"
+  };
+  url = url + "?origin=*";
+  Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
+  return fetch(url, { mode: 'cors' })
+    .then(function (response) { return response.json(); })
+    .catch(function (error) { console.log(error); });
 }
 
 
 
 export function scrapeWikiPage(string) {
-	return new Promise((resolve, reject) => {
-		request(`https://en.wikipedia.org/wiki/${string}`, function (err, res, html) {
-			if (!err && res.statusCode === 200) {
-				const nodes = []
-				let $ = cheerio.load(html);
-				let childrenNodes = $('p').find('a').slice(0, 2)
-				childrenNodes.each(function (i, elem) {
-					nodes[i] = $(this).text();
-				});
-				resolve(nodes)} else {
-				reject(err)
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    request(`https://en.wikipedia.org/wiki/${string}`, function (err, res, html) {
+      if (!err && res.statusCode === 200) {
+        const nodes = []
+        let $ = cheerio.load(html);
+        let childrenNodes = $('p').find('a').slice(0, 2)
+        childrenNodes.each(function (i, elem) {
+          nodes[i] = $(this).text();
+        });
+        resolve(nodes)
+      } else {
+        reject(err)
+      }
+    });
+  });
 }
