1b505b96124ad573efcffbe39a6e3d248a1b131a
Chi Huynh <chi@skyhawk-277gt.local>
2019-10-09 11:31:49 -0700

    convert indentation to spaces

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -6,24 +6,24 @@ import './Container.scss'
 
 
 const Container = (props) => (
-	<>
+  <>
 
-		<NavBar user={props.user} handleLogout={props.handleLogout} />
-		<SearchBar
-			id='searchBar'
-			handleSearch={props.handleSearch}
-			handleChange={props.handleChange}
-			value={props.value} />
-		{(props.nodes) ? props.nodes.map((node, idx) => (
-			<div>
-				<button className='nodes' onClick={() => props.handleNodeClick(node.name)}>
-					<Node key={idx} node={node} />
-				</button>
-				{node.snippet}
-			</div>
-		)) : ''
-		}
-	</>
+    <NavBar user={props.user} handleLogout={props.handleLogout} />
+    <SearchBar
+      id='searchBar'
+      handleSearch={props.handleSearch}
+      handleChange={props.handleChange}
+      value={props.value} />
+    {(props.nodes) ? props.nodes.map((node, idx) => (
+      <div>
+        <button className='nodes' onClick={() => props.handleNodeClick(node.name)}>
+          <Node key={idx} node={node} />
+        </button>
+        {node.snippet}
+      </div>
+    )) : ''
+    }
+  </>
 );
 
 
