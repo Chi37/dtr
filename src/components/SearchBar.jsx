bce003b801588a8f054a519e0e9f3c6dc4ead331
Chi Huynh <chi@skyhawk-277gt.local>
2019-10-11 13:36:49 -0700

    wiki search format button to be displayed in flex row direction

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const SearchBar = (props) => {
 	return (
 		<>
-			<form onSubmit={props.handleSearch}>
+			<form className='wiki-search' onSubmit={props.handleSearch}>
 				<input placeholder="search Wikipedia" type="text" autoComplete="off" onChange={props.handleChange} />
-				<button id='searchBtn'><i class='search-icon'>o</i></button>
+				<button id='searchBtn'>
+					Search
+				</button>
 			</form>
 		</>
 	);
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -73,34 +73,38 @@ class App extends Component {
           <NavBar user={this.state.user} handleLogout={this.handleLogout} />
         </header>
         <Switch>
-          <Route exact path="/" render={() => (
-            userService.getUser() ?
-            <Container
-              user={this.state.user}
-              value={this.state.value}
-              nodes={this.state.nodes}
-              handleLogout={this.handleLogout}
-              handleSearch={this.handleSearch}
-              handleChange={this.handleChange}
-              handleNodeClick={this.handleNodeClick}
-            /> 
-            :
-            <Redirect to='/login'/>)}
-          />
-          <Route exact path="/signup" render={({ history }) => (
-            <SignupPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />)}
-          />
-          <Route exact path="/login" render={({ history }) => (
-            <LoginPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />)}
-          />
+          <div className='container'>
+            <Route exact path="/" render={() => (
+              userService.getUser() ?
+                <Container
+                  user={this.state.user}
+                  value={this.state.value}
+                  nodes={this.state.nodes}
+                  handleLogout={this.handleLogout}
+                  handleSearch={this.handleSearch}
+                  handleChange={this.handleChange}
+                  handleNodeClick={this.handleNodeClick}
+                />
+                :
+                <Redirect to='/login' />)}
+            />
+            <Route exact path="/signup" render={({ history }) => (
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />)}
+            />
+            <Route exact path="/login" render={({ history }) => (
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />)}
+            />
+          </div>
         </Switch>
-        <footer>Made by Chi  <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></footer>
+        <footer>Made by Chi
+        <div> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        </footer>
       </>
     );
   }
